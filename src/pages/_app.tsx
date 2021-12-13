ed56b6b3e5a72ac8c27b4118a4cf7ccae07bb106
David Alexander <me@dja.dev>
2021-12-13 23:18:37 +0000

    chore(styles): remove @styles folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,5 @@
 import { AppProps } from 'next/app';
 
-import '@/styles/globals.css';
-// !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
-import '@/styles/colors.css';
-
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
